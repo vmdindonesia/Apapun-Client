e807da56b7fd9cb6bda8f23993aede78376b9795
REN <rendy1429@gmail.com>
2018-06-21 21:24:49 +0700

    update

--- src/pages/login.js
+++ src/pages/login.js
@@ -62,23 +62,14 @@ export class LoginPage extends React.Component {
 				}
 			}).then(response => {
 				console.log(response.data, 'Data Login');
-				const IdUser = response.data.userId;
-				AsyncStorage.setItem('VMDDEVELOPER', IdUser.toString(), (err, result) => {
-					console.log(result, 'Sukses Storage');
-					console.log(err, 'error storage');
+				const IdUser = response.data;
+				AsyncStorage.setItem('VMDDEVELOPER', JSON.stringify(IdUser), () => {
+					const resetAction = StackActions.reset({
+						index: 0,
+						actions: [NavigationActions.navigate({ routeName: 'Dashboard' })],
+					});
+					this.props.navigation.dispatch(resetAction);
 				});
-				// try {
-				// 	const dataLogin = response.data;
-				// 	await AsyncStorage.setItem('VMDDEVELOPER', JSON.parse(dataLogin));
-				// } catch (error) {
-				// 	console.log(error, 'Error Saving Storage');
-				// 	// Error saving data
-				// }
-				const resetAction = StackActions.reset({
-					index: 0,
-					actions: [NavigationActions.navigate({ routeName: 'Dashboard' })],
-				});
-				this.props.navigation.dispatch(resetAction);
 			}).catch(error => {
 				console.log(error, 'ERROR LOGIN');
 				this.setState({ loading: false });
