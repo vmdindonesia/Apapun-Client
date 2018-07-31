17e5a718e06eaac85d86f74b510f9d50dee9b941
Acis <akislatuconsina@gmail.com>
2018-08-01 00:25:46 +0700

    update revision page

--- src/pages/HelpMenu.js
+++ src/pages/HelpMenu.js
@@ -28,7 +28,7 @@ export class HelpMenuPage extends React.Component {
                         width: '100%',
                     }}>
                         <ImageBackground
-                            style={{ width: '100%', height: '100%' }}
+                            style={{ flex: 1 }}
                             source={require('./../assets/images/help_top_background.jpg')}
                             resizeMode='cover'
                         >
@@ -50,7 +50,7 @@ export class HelpMenuPage extends React.Component {
                                     <InputSearch
                                         // onFocus={() => navigate('FilterBefore')}
                                         placeholder="Cari Pertanyaan atau Kata Kunci"
-                                        icon="ic_search"
+                                        // icon="ic_search"
                                     />
                                 </View>
                             </View>
