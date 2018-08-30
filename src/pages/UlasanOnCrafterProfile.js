f3bebae3e94cf2be684ecfdbeb0840c2ae35e0b4
RENDY <rendy1429@gmail.com>
2018-08-31 05:34:44 +0700

    update ulasan

--- src/pages/UlasanOnCrafterProfile.js
+++ src/pages/UlasanOnCrafterProfile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { ToastAndroid, View, Text, ImageBackground, Image, AsyncStorage, TouchableOpacity, TextInput, ScrollView, StyleSheet, TouchableHighlight, TouchableWithoutFeedback, StatusBar, Modal } from 'react-native'
-// import axios from 'axios';
+import { ToastAndroid, View, Text, FlatList, Image, AsyncStorage, TouchableOpacity, TextInput, ScrollView, StyleSheet, TouchableHighlight, TouchableWithoutFeedback, StatusBar, Modal } from 'react-native'
+import axios from 'axios';
+import { IPSERVER } from '../shared/config';
+import moment from 'moment';
 import Swiper from 'react-native-swiper';
 import { Container, ContainerSection, Button, Input, InputDate, InputNumber } from '../components/common';
 
@@ -21,9 +23,38 @@ export class UlasanOnCrafterProfilePage extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            // screen: 'Custom'
-
+            crafterId: '',
+            dataCommentar: '',
+            dataTotal: ''
         }
+    }
+
+    componentDidMount() {
+        this.setState({
+            crafterId: this.props.navigation.state.params.crafter_Id
+        }, () => {
+            axios.post(`${IPSERVER}/ApapunReviews/getReviewByCrafterId`, {
+                crafterId: this.state.crafterId
+            })
+                .then(response => {
+                    console.log(response, 'Data Commentar')
+                    this.setState({ dataCommentar: response.data }, () => {
+                        axios.post(`${IPSERVER}/ApapunReviews/getTotalReviewByCrafterId`, {
+                            crafterId: this.state.crafterId
+                        })
+                            .then(response => {
+                                console.log(response, 'Data Total')
+                                this.setState({ dataTotal: response.data });
+                            }).catch(error => {
+                                console.log(error, 'Error Total');
+                                return ToastAndroid.show('Connection Time Out, Server Maybe Down', ToastAndroid.SHORT);
+                            });
+                    });
+                }).catch(error => {
+                    console.log(error, 'Error Commentar');
+                    return ToastAndroid.show('Connection Time Out, Server Maybe Down', ToastAndroid.SHORT);
+                });
+        });
     }
 
     renderProductImage = (data) => {
@@ -39,6 +70,130 @@ export class UlasanOnCrafterProfilePage extends React.Component {
                 </View>
 
             </TouchableOpacity >
+        )
+    }
+
+
+    renderCommentar = (data) => {
+        console.log(data, '098');
+        return (
+            <View style={{ flex: 1, backgroundColor: 'white', marginTop: 12.5, marginLeft: 15, marginRight: 15 }}>
+
+                <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10, marginRight: 20, marginLeft: 20 }}>
+
+                    <View style={{ width: '60%', flexDirection: 'row', alignItems: 'center', }}>
+                        <View style={{ height: 65, width: 65, borderRadius: 100, backgroundColor: 'black' }}>
+                            <Image
+                                style={{
+                                    width: 50,
+                                    height: 50,
+                                    alignSelf: 'center',
+                                    borderRadius: 50
+                                }}
+                                resizeMode='stretch'
+                                source={{ uri: `${IPSERVER}/ApapunStorageImages/images/download/${data.item.profile_url === undefined ? 'https://www.coastalsocks.com.ng/wp-content/uploads/2014/04/default-avatar.png' : data.item.profile_url}` }}
+                            />
+                        </View>
+
+                        <View style={{ justifyContent: 'center', flex: 1 }}>
+                            <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', paddingLeft: 10 }}>{data.item.realm}</Text>
+                            <Text style={{ fontFamily: 'Quicksand-Regular', fontSize: 13, color: 'black', paddingLeft: 10 }}>{moment(data.item.created_at).format('DD MMMM YYYY, h:mm')}</Text>
+                        </View>
+                    </View>
+                    <View style={{ width: '40%', height: '100%', flexDirection: 'row' }}>
+                        {
+                            data.item.rating === 1 ?
+                                <Image
+                                    style={{
+                                        width: 50,
+                                        height: 50,
+                                        alignSelf: 'center',
+                                        marginLeft: 50
+                                    }}
+                                    resizeMode='contain'
+                                    source={require('./../assets/images/Buruk.png')}
+                                />
+                                :
+                                data.item.rating === 2 ?
+                                    <Image
+                                        style={{
+                                            width: 50,
+                                            height: 50,
+                                            alignSelf: 'center',
+                                            marginLeft: 50
+                                        }}
+                                        resizeMode='contain'
+                                        source={require('./../assets/images/Cukup.png')}
+                                    />
+                                    :
+                                    data.item.rating === 3 ?
+                                        <Image
+                                            style={{
+                                                width: 50,
+                                                height: 50,
+                                                alignSelf: 'center',
+                                                marginLeft: 50
+                                            }}
+                                            resizeMode='contain'
+                                            source={require('./../assets/images/Bagus.png')}
+                                        />
+                                        :
+                                        data.item.rating === 4 ?
+                                            <Image
+                                                style={{
+                                                    width: 50,
+                                                    height: 50,
+                                                    alignSelf: 'center',
+                                                    marginLeft: 50
+                                                }}
+                                                resizeMode='contain'
+                                                source={require('./../assets/images/sempurna.png')}
+                                            />
+                                            :
+                                            <View />
+                        }
+                    </View>
+                </View>
+                <View style={{ margin: 5, height: '80%', }}>
+                    <View style={{}}>
+                        <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 15, color: 'black', paddingLeft: 10 }}>{data.item.title}</Text>
+                    </View>
+                    <View style={{ paddingTop: 5, flex: 1 }}>
+                        <Text style={{ fontFamily: 'Quicksand-Regular', fontSize: 13, color: 'black', paddingLeft: 10 }}>{data.item.description}</Text>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+
+    renderTotal = (data) => {
+        console.log(data, 'Total');
+        return (
+            <View style={{ width: '100%', height: 30, flexDirection: 'row', backgroundColor: 'red' }}>
+                <View style={{ width: '25%', flexDirection: 'column', alignItems: 'center' }}>
+                    <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', alignSelf: 'center' }}>({data.item.rating === 1 ? data.item.jml_rating : 0})</Text>
+
+                </View>
+
+                <View style={{ width: '25%', flexDirection: 'column', alignItems: 'center' }}>
+
+                    <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', alignSelf: 'center' }}>({data.item.rating === 2 ? data.item.jml_rating : 0})</Text>
+
+                </View>
+
+                <View style={{ width: '25%', flexDirection: 'column', alignItems: 'center' }}>
+
+                    <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', alignSelf: 'center' }}>({data.item.rating === 3 ? data.item.jml_rating : 0})</Text>
+
+                </View>
+
+                <View style={{ width: '25%', flexDirection: 'column', alignItems: 'center' }}>
+
+                    <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', alignSelf: 'center' }}>({data.item.rating === 4 ? data.item.jml_rating : 0})</Text>
+
+                </View>
+
+            </View>
         )
     }
 
@@ -102,99 +257,21 @@ export class UlasanOnCrafterProfilePage extends React.Component {
                     </View>
                 </View>
 
-                <View style={{ width: '100%', height: 30, flexDirection: 'row', backgroundColor: 'white' }}>
-                    <View style={{ width: '25%', flexDirection: 'column', alignItems: 'center' }}>
+                <FlatList
+                    data={this.state.dataTotal}
+                    renderItem={this.renderTotal.bind(this)}
+                    showsHorizontalScrollIndicator={false}
+                    horizontal={false}
+                    numColumns={4}
+                />
 
-                        <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', alignSelf: 'center' }}>(0)</Text>
-
-                    </View>
-
-                    <View style={{ width: '25%', flexDirection: 'column', alignItems: 'center' }}>
-
-                        <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', alignSelf: 'center' }}>(1)</Text>
-
-                    </View>
-
-                    <View style={{ width: '25%', flexDirection: 'column', alignItems: 'center' }}>
-
-                        <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', alignSelf: 'center' }}>(2)</Text>
-
-                    </View>
-
-                    <View style={{ width: '25%', flexDirection: 'column', alignItems: 'center' }}>
-
-                        <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', alignSelf: 'center' }}>(4)</Text>
-
-                    </View>
-
+                <View style={{ flex: 1, backgroundColor: 'red', flexDirection: 'row' }}>
+                    <FlatList
+                        data={this.state.dataCommentar}
+                        renderItem={this.renderCommentar.bind(this)}
+                        extraData={this.state}
+                    />
                 </View>
-
-
-
-                <ScrollView style={{
-                    backgroundColor: '#eaeaea',
-                    flex: 1
-                }}>
-
-                    <View style={{ flex: 1, backgroundColor: 'white', marginTop: 12.5, marginLeft: 15, marginRight: 15 }}>
-
-                        <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10, marginRight: 20, marginLeft: 20 }}>
-
-                            <View style={{ width: '60%', flexDirection: 'row', alignItems: 'center', }}>
-                                <View style={{ height: 65, width: 65, borderRadius: 100, backgroundColor: 'black' }}>
-                                    {/* <Image
-                                        style={{
-                                            width: 50,
-                                            height: 50,
-                                            alignSelf: 'center',
-                                            borderRadius: 50
-                                        }}
-                                        resizeMode='stretch'
-                                        source={require('./../assets/images/profile.png')}
-                                    /> */}
-                                </View>
-
-                                <View style={{ justifyContent: 'center', flex: 1 }}>
-                                    <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 13, color: 'black', paddingLeft: 10 }}>Gal Gadot</Text>
-                                    <Text style={{ fontFamily: 'Quicksand-Regular', fontSize: 13, color: 'black', paddingLeft: 10 }}>28 Januari 2018, 18.04</Text>
-                                </View>
-                            </View>
-                            <View style={{ width: '40%', height: '100%', flexDirection: 'row' }}>
-                                <Image
-                                    style={{
-                                        width: 50,
-                                        height: 50,
-                                        alignSelf: 'center',
-                                        marginLeft: 50
-                                    }}
-                                    resizeMode='contain'
-                                    source={require('./../assets/images/sempurna.png')}
-                                />
-                            </View>
-                        </View>
-
-                        <View style={{ margin: 5, height: '80%', }}>
-
-                            <View style={{}}>
-                                <Text style={{ fontFamily: 'Quicksand-Bold', fontSize: 15, color: 'black', paddingLeft: 10 }}>Rekomen Banget</Text>
-                            </View>
-                            <View style={{ paddingTop: 5, flex: 1 }}>
-                                <Text style={{ fontFamily: 'Quicksand-Regular', fontSize: 13, color: 'black', paddingLeft: 10 }}>Kecocokan barang dengan barang yang saya buat cukup memuaskan Kecocokan barang dengan barang yang saya buat cukup memuaskanKecocokan barang dengan barang yang saya buat cukup memuaskan.</Text>
-                            </View>
-                        </View>
-
-                    </View>
-
-
-
-
-
-
-
-
-                </ScrollView >
-
-
             </View >
         )
     }
